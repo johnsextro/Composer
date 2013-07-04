18fa9f848d27bb5a38327b8074d06ca9bbf200b5
Family <family@Familys-iMac.local>
2013-07-04 11:33:21 -0500

    Switching to using chrome allowed me to be able to click the button as a DIV.

--- src/test/java/composerwindow/test/ComposerUIStepDefs.java
+++ src/test/java/composerwindow/test/ComposerUIStepDefs.java
@@ -10,8 +10,10 @@ import cucumber.api.java.en.*;
 
 
 public class ComposerUIStepDefs {
-	protected DefaultSelenium seleniumClient = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080");
+	protected DefaultSelenium seleniumClient = new DefaultSelenium("localhost", 4444, "*googlechrome", "http://localhost:8080");
+	private MyWaiter waiter = new MyWaiter(seleniumClient);
 
+	
 	@When("^the application starts$")
 	public void the_application_starts() throws Throwable {
 		seleniumClient.start();
@@ -20,14 +22,10 @@ public class ComposerUIStepDefs {
 
 	@Then("^I see the Click Me button$")
 	public void checkClickMe() throws Throwable {
-		int attempt = 0;
-		while (!seleniumClient.isElementPresent("//span[contains(@class, 'v-button-wrap')]") && attempt < 10){
-			attempt++;
-			Thread.sleep(500);
-		}
+		waiter.waitForElement("//span[contains(@class, 'v-button-wrap')]", 10, MyWaiter.HALF_SECOND);
 		assertTrue(seleniumClient.isElementPresent("//span[contains(@class, 'v-button-wrap')]"));
 	}
-	
+
 	@When("^I click the Click Me button$")
 	public void I_click_the_Click_Me_button() throws Throwable {
 		seleniumClient.click("//*[@id='Composer-534698688']/div/div[2]/div[1]/div");
@@ -35,7 +33,8 @@ public class ComposerUIStepDefs {
 
 	@Then("^I will see Thank you for clicking$")
 	public void I_will_see_Thank_you_for_clicking() throws Throwable {
-	    assertTrue(seleniumClient.isTextPresent("Thank you for clicking"));
+		waiter.waitForText("Thank you for clicking", 10, MyWaiter.HALF_SECOND);
+		assertTrue(seleniumClient.isTextPresent("Thank you for clicking"));
 	}
 	
 	@After
---
+++ src/test/java/composerwindow/test/MyWaiter.java
@@ -0,0 +1,33 @@
+package test.java.composerwindow.test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+
+public class MyWaiter {
+	public static final int HALF_SECOND = 500;
+	public static final int ONE_SECOND = 1000;
+	public static final int QUARTER_SECOND = 250;
+	public static final int ONE_TENTH_SECOND = 100;
+	
+	private DefaultSelenium seleniumClient;
+
+	public MyWaiter(DefaultSelenium seleniumClient) {
+		super();
+		this.seleniumClient = seleniumClient;
+	}
+	
+	public void waitForElement(String locator, int maxRetryAttempts, int millsToWaitBetweenAttempts) throws InterruptedException {
+		int attempt = 0;
+		while (!seleniumClient.isElementPresent(locator) && attempt < maxRetryAttempts){
+			attempt++;
+			Thread.sleep(millsToWaitBetweenAttempts);
+		}
+	}
+
+	public void waitForText(String textToWaitFor, int maxRetryAttempts, int millsToWaitBetweenAttempts) throws InterruptedException {
+		int attempt = 0;
+		while (!seleniumClient.isTextPresent(textToWaitFor) && attempt < maxRetryAttempts){
+			attempt++;
+			Thread.sleep(millsToWaitBetweenAttempts);
+		}
+	}
+}
